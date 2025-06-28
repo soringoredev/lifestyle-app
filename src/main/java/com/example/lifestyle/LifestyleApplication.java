5eaa194cdb70c28fdea79a8dcd443b423ffacda3
Sorin Gore <soringoredev@gmail.com>
2025-06-28 20:32:55 +0300

    Initial commit

---
+++ src/main/java/com/example/lifestyle/LifestyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifestyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lifestyle/LifestyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifestyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifestyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
